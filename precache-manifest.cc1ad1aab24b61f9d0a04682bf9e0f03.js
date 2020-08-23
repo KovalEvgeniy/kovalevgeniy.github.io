a894637622ab18f5c76c1be6d1420fe2ceb9af93
KovalEvgeniy <evgeniykovalnew@gmail.com>
2020-08-23 20:45:40 +0300

    upload compiled code

---
+++ precache-manifest.cc1ad1aab24b61f9d0a04682bf9e0f03.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "589849b9f2b5fdea2ecbefddd5b19a9b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "781fe67eef2257b39813",
+    "url": "/static/css/2.8ff17cee.chunk.css"
+  },
+  {
+    "revision": "08c5727028408f260d6c",
+    "url": "/static/css/main.3ac8028d.chunk.css"
+  },
+  {
+    "revision": "781fe67eef2257b39813",
+    "url": "/static/js/2.22e17854.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.22e17854.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08c5727028408f260d6c",
+    "url": "/static/js/main.8a30459a.chunk.js"
+  },
+  {
+    "revision": "1ca56b6e53ce1dc83ae6",
+    "url": "/static/js/runtime-main.0a3c20bd.js"
+  }
+]);
